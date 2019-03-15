69b3846de4fdc348444134f381a06a760656a723
Karnankaviya <karnan.kali@yahoo.com>
2019-03-15 12:51:32 +0100

    Update App.js
    Added new statement

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Karnan is an Indian.
+          Karnan is an Indian. He is the BEST
         </p>
       </div>
     );
